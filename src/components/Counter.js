54d2a846e3a03c00ededf504af03157f90718a91
Zsolt Vizi <vizi.g.zsolt@gmail.com>
2020-07-27 11:09:53 +0300

    Add counter and test with vue test utils

---
+++ src/components/Counter.js
@@ -0,0 +1,13 @@
+export default {
+    template: `
+        <div>
+            <span class="count" v-text="count"></span>
+        </div>
+    `,
+
+    data () {
+        return {
+            count: 0
+        }
+    }
+}
---
+++ test/counter.spec.js
@@ -0,0 +1,19 @@
+import { mount } from "@vue/test-utils";
+import Counter from "../src/components/Counter";
+import expect from 'expect';
+
+describe ('Counter', () => {
+
+    it ('defaults to a count of 0', () => {
+        let wrapper = mount(Counter);
+
+        expect(wrapper.vm.count).toBe(0);
+    });
+
+    it ('displays the current count', () => {
+        let wrapper = mount(Counter);
+
+        expect(wrapper.find('.count').html()).toContain(0)
+    });
+
+});
