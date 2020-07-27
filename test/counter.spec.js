52f22ac16c32e0577bebb24e83216dbc3ceba2d7
Zsolt Vizi <vizi.g.zsolt@gmail.com>
2020-07-27 15:16:11 +0300

    Add further tests for counter

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -1,4 +1,4 @@
-import { mount } from "@vue/test-utils";
+import {mount} from "@vue/test-utils";
 import Counter from "../src/components/Counter.vue";
 import expect from 'expect';
 
@@ -17,12 +17,22 @@ describe ('Counter', () => {
         expect(wrapper.find('.count').html()).toContain(0)
     });
 
-    it ('increments the count when the button is clicked', () => {
+    it ('increments the count when the increment button is clicked', () => {
         expect(wrapper.vm.count).toBe(0);
 
-        wrapper.find('button').trigger('click')
+        wrapper.find('.increment').trigger('click')
 
         expect(wrapper.vm.count).toBe(1);
+    });
+
+    it ('decrements the count when the decrements button is clicked', () => {
+        wrapper.setData({
+            count: 5
+        })
+
+        wrapper.find('.decrement').trigger('click')
+
+        expect(wrapper.vm.count).toBe(4);
     });
 
     it ('displays the current count', async () => {
@@ -33,4 +43,23 @@ describe ('Counter', () => {
         expect(wrapper.find('.count').html()).toContain(1)
     });
 
+    it ('never goes below 0', () => {
+        expect(wrapper.vm.count).toBe(0);
+
+        wrapper.find('.decrement').trigger('click')
+
+        expect(wrapper.vm.count).toBe(0);
+    })
+
+    it ('won`t display decrement button when counter is at 0', async () => {
+        await wrapper.setData({
+            count: 1
+        })
+
+        expect(wrapper.find('.decrement').isVisible()).toBe(true);
+
+        await wrapper.find('.decrement').trigger('click')
+
+        expect(wrapper.find('.decrement').isVisible()).toBe(false);
+    })
 });
