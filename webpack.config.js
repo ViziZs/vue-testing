a987587b496959cf37e353a912308739a1d5eb28
Zsolt Vizi <vizi.g.zsolt@gmail.com>
2020-07-27 12:50:36 +0300

    Change from js file to an actual vue component
    Change the webpack.config.js to use vue loader for pre-compile before testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
-var nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals');
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
     mode: 'development',
-    target: "node", // webpack should compile node compatible code
-    externals: [nodeExternals()] // in order to ignore all modules in node_modules folder
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 }
